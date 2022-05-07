19eeee1285d46e6f22f5a322e42911ab77e9df8c
andrehahn05 <andrehahn05@gmail.com>
2022-05-07 16:49:25 -0400

    ListView

--- lib/components/tarnsaction_form.dart
+++ lib/components/tarnsaction_form.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
 class TransactionForm extends StatelessWidget {
-  TransactionForm({Key? key}) : super(key: key);
+  TransactionForm(this.onSubmit, {Key? key} ) : super(key: key);
   final titleController = TextEditingController();
   final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +38,9 @@ class TransactionForm extends StatelessWidget {
                     ),
                   ),
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0;
+                    onSubmit(title, value);
                   },
                 )
               ],
--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'tarnsaction_form.dart';
 import 'transaction_list.dart';
@@ -24,14 +26,74 @@ class _TransactionUserState extends State<TransactionUser> {
       value: 249.30,
       date: DateTime.now(),
     ),
+    Transaction(
+      id: 't3',
+      title: 'Conta #1',
+      value: 249.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't4',
+      title: 'Conta #2',
+      value: 500.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't5',
+      title: 'Conta #3',
+      value: 200.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Conta #4',
+      value: 249.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't7',
+      title: 'Conta #5',
+      value: 249.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't8',
+      title: 'Conta #6',
+      value: 500.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't9',
+      title: 'Conta #7',
+      value: 200.30,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't6',
+      title: 'Conta #4',
+      value: 249.30,
+      date: DateTime.now(),
+    ),
   ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
+        TransactionForm(_addTransaction),
         TransactionList(_transactions),
-        TransactionForm(),
       ],
     );
   }
